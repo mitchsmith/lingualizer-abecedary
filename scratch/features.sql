d0337d6bd35241e26592e83cbbd6a1a604264016
Mitch Smith <mitch_smith@wgbh.org>
2013-09-30 08:07:37 -0500

    More distinctive feature attibutes

--- scratch/features.sql
+++ scratch/features.sql
@@ -1,3 +1,7 @@
+-- The Articulatory Correlates of the Distinctive Features
+-- (from Halle and Clements’ Problem Book in Phonology) 
+-- https://www.msu.edu/course/asc/232/DF/ACofDFs.html
+
 update segments set sylabic = 1 where ipa_type = "vowel";
 update segments set sylabic = 0 where ipa_type != "vowel";
 select id, ipa_symbol, description, sylabic from segments;
@@ -25,3 +29,17 @@ update segments set voice = 1 where voice is NULL and sonorant = 1;
 select id, ipa_symbol, description, sylabic, consonantal, aproximant, sonorant where sonorant = true and (description like "%fricative%" or description like "%sibilant");
 update segments set continuant = 1 where description not like "%affricate%" and (description like "%fricative%" or description like "%sibilant%");
 update segments set continuant = 1 where sonorant = 1;
+update segments set continuant = 0 where contiunant is NULL;
+
+select id, ipa_symbol, description from segments where desctiption like "%nasal%";
+update segments set nasal = 1 where description like "%nasal%";
+
+select id, ipa_symbol, description from segments where description like "%lateral%";
+update segments set lateral = 1 where description like "%lateral%";
+update segments set lateral = 0 where lateral is NULL;
+
+select id, ipa_symbol, description from segments where (description like "%sibilant%" or description like "%fric%") and (description like "% sibilant%" or description like "%labiodental%" or description like "%uvular%");
+update segments set strident = 1 where (description like "%sibilant%" or description like "%fric%") and (description like "% sibilant%" or description like "%labiodental%" or description like "%uvular%");
+update segments set strident = 0 where strident is NULL;
+
+
